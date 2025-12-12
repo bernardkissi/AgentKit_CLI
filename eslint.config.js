f0d82f07c2640baa7b6e69c04e159d8da1eebda4
bernard.kissi <bernard.kissi@gleif.org>
2025-12-12 09:39:32 +0100

    feat: add some project tooling

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const globals = require("globals");
+
+module.exports = [
+  {
+    ignores: ["dist/**", "node_modules/**"]
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      },
+      globals: {
+        ...globals.es2022,
+        ...globals.node
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+    }
+  },
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts", "**/test/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      },
+      globals: {
+        ...globals.es2022,
+        ...globals.node,
+        ...globals.jest,
+        vi: "readonly"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+    }
+  }
+];
